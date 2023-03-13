47c70a21f5ec57d11b26ccce0e87175e3f2362fb
Abran Lezama <lezama.abran@gmail.com>
2023-03-13 16:41:13 -0700

    updated song card size to make it more responsive

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -6,7 +6,6 @@ import PlayPause from './PlayPause';
 import { playPause, setActiveSong } from '../redux/features/playerSlice';
 
 const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
-
   const dispatch = useDispatch();
 
   const handlePauseClick = () => {
@@ -18,9 +17,9 @@ const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
     dispatch(playPause(true));
   };
 
- return (
-    <div className="flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
-      <div className="relative w-full h-56 group">
+  return (
+    <div className="flex flex-col w-[250px] xl:w-[180px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer">
+      <div className="relative w-full h-56 xl:h-40 group">
         <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.title === song.title ? 'flex bg-black bg-opacity-70' : 'hidden'}`}>
           <PlayPause
             isPlaying={isPlaying}
