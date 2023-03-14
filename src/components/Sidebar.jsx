b8185658168b8151748d6505e5ee407280d31f73
Abran Lezama <lezama.abran@gmail.com>
2023-03-13 20:31:19 -0700

    added application log to sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 import { HiOutlineMenu } from "react-icons/hi";
 import { RiCloseLine } from 'react-icons/ri'
 
-import { logo } from '../assets'
+import { musicLogo } from '../assets'
 import { links } from '../assets/constants'
 
 const NavLinks = ({ handleClick }) => (
@@ -30,7 +30,7 @@ const Sidebar = () => {
   return (
     <>
       <div className='md:flex hidden flex-col w-[170px] py-10 px-4 bg-[#191624]'>
-        <img src={logo} alt='logo' className='w-full h-14 object-contain' />
+        <img src={musicLogo} alt='logo' className='w-full h-14 object-contain' />
         <NavLinks />
       </div>
       <div className='absolute md:hidden block top-6 right-3'>
@@ -45,7 +45,7 @@ const Sidebar = () => {
       </div>
       <div className={`absolute top-0 h-screen w-2/3 bg-gradient-to-tl from-white/10 to-[#483d8b] 
         backdrop-blur-lg z-10 p-6 md:hidden smooth-transition ${mobileMenuOpen ? 'left-0' : '-left-full'}`}>
-        <img src={logo} alt='logo' className='w-full h-14 object-contain' />
+        <img src={musicLogo} alt='logo' className='w-full h-14 object-contain' />
         <NavLinks handleClick={() => setMobileMenuOpen(false)} />
       </div>
     </>
